b123673c1d297ffc84e230e5426d8c4e5d8310bc
Neto Pompeu <neto1@hotmail.fr>
2021-05-03 12:25:29 +0200

    this work now

--- server.js
+++ server.js
@@ -1,43 +1,59 @@
 const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 
-app.use(express.urlencoded());
 
-    //- Connect you mongodb Compass
+    //-[EN] use Express instead of Body-Parser //[PF]- Use Express au lieu de BodyParser
+app.use(express.urlencoded({extended: true}));
+//app.use(express.json()); //-[EN] I don't know what it's for //[PF]- Nao sei para o que serve
+
+    //-[EN] Connect you mongodb Compass
 mongoose.connect( 
-    "mongodb://localhost:27017/node-api-mvp",
+    "mongodb://localhost:27017/html-mongo",
     { useNewUrlParser: true, useUnifiedTopology: true },
     (err) => {
         if(!err) console.log("Mongodb connected");
         else console.log("Connection error :" + err);
     });
 
-    //-Create a data schema
+    //-[EN] Create a data schema
     const notesSchema = {
         title: String,
         content: String
     }
     
-    const Note = mongoose.model("node-api-mvp", notesSchema,"posts");
+        //-[EN] PostModel   //-[FR] html-mongo is you dataBase, NoteSchema is you data schema, posts is you table where you save the data
+    const Note = mongoose.model('html-mongo', notesSchema, 'posts');
 
-    //- Communicate with the file index.html
-app.get('/', function(req,res) {
+    //-[EN] Communicate with the file index.html
+app.get('/', function(req,res)  {
     res.sendFile('/Users/pompeu/project/html-mongo/index.html');
-})
-
-    //- Send data to Mongo 
-app.post("/", function(req,res) {
-    let newNote = new Note ({
-        title: req.body.title,
-        content: req.body.content
-    });
-    newNote.save();
-    res.redirect('/');
-
 });
 
+    //-[EN] Send data to Mongo and recup the input data (name = title, content)
+app.post("/", function(req,res)  {
+    console.log(req.body)
+  
+     let newNote = new Note ({
+     title: req.  body.title,
+      content: req.body.content
+    });
+
+    //- Save the data on MongoDb
+ newNote.save().then(() => {
+
+    console.log('save with sucess' + newNote)
+    
+    }).catch((err) => {
+        console.log('No save because' + err)
+    });
+
+
+        //-[EN] This make it possible not to crash the website //-[PF] Permet de ne pas faire crash le site 
+        res.redirect('/');
+});
+
+    //-[EN] index.html reading Port  //-[PF] Port de lecture du index.html
 app.listen(3000, function() {
         console.log("server is running on 3000");
 });
